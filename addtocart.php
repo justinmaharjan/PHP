ab1a14094cff4fc3ff8711aba48f17dcb9bc74a7
Justin <mhrjastin@gmail.com>
2024-03-03 10:55:21 +0545

    php

---
+++ addtocart.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<?php
+session_start();
+$item = $_POST["shopping"];
+
+
+if (isset($_SESSION["array"])) {
+  array_push($_SESSION["array"],$item);
+}
+else {
+  $_SESSION["array"] = [$item];
+}
+
+
+// settinf data on session
+var_dump($_SESSION["array"]) ;
+header("location: /");
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,14 +1,38 @@
+<?php
+session_start();
+$shoppingList =$_SESSION["array"] ?? [];
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>shopping</title>
 </head>
 <body>
-    <h1>justin</h1>
-    <?php
-    echo"justin";
-    ?>
+  <main>
+    <h1>Shopping Items</h1>
+    <!-- form to submit -->
+    <section>
+      <form action="./add_to_cart.php" method="post">
+        <input type="text" name = "shopping">
+        <button type="submit">add to cart</button>
+      </form>
+    </section>
+    <!-- list -->
+    <section>
+      <h4>Shopping list here</h4>
+      <ul>
+        <?php 
+        foreach($shoppingList as $element){
+          echo "<li>{$element}</li>";
+        }
+        ?>
+      </ul>
+    </section>
+</main>
 </body>
 </html>
