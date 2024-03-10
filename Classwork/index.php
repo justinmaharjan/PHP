ab5dd57f3b6b5ac8dd71000fa285a4aee02d5803
Justin <mhrjastin@gmail.com>
2024-03-10 11:08:25 +0545

    added

--- Classwork/index.php
+++ Classwork/index.php
@@ -3,37 +3,31 @@ require './connect.php';
 $sqlQuery = 'select * from contacts';
 $phone_data = mysqli_query($conn,$sqlQuery);
 $result = mysqli_fetch_all($phone_data,MYSQLI_ASSOC);
-var_dump($result);
+// var_dump($result);
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Phone Book System</title>
+    <title>Document</title>
 </head>
 <body>
-    <h1>Welcome to phone management system</h1>
-    <form action="" method="post">
-        <input type='text' name='contact'>
+    <h1>Phone management system</h1>
+    <form action="add_contact.php" method="post">
+        <input type='text' name='first-name' placeholder='first name'>
+        <input type='text' name='middle-name' placeholder='middle name'>>
+        <input type='text' name='last-name' placeholder='last name'>>
+        <input type='text' name='phone' placeholder='phone'>>
+
         <br>
-        <form action="" method="post">
         <button type="submit">Add</button>
-        </form>
-        <form action="" method="post">
-        <button type="submit">Edit</button>
-        </form>
-        <form action="" method="post">
-        <button type="submit">Delete</button>
-        </form>
     </form>
     <div>
         <ul>
             <?php
-            foreach ($result["first_name"] as $d) {
-                echo "<li>$d</li>";
+            foreach ($result as $d) {
+                echo "<li>{$d['first_name']}  {$d['middle_name']} {$d['last_name']}    {$d['phone_number']}</li>";
             }
             
             ?>
